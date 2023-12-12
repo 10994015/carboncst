affd15954cb4057f60ca4405b30a592f6e0d7c6f
10994015 <a0938599191@gmail.com>
2023-12-12 09:39:50 +0800

    理事長的話 改用orderby  created_at

--- app/Http/Livewire/ChairmanComponet.php
+++ app/Http/Livewire/ChairmanComponet.php
@@ -9,7 +9,7 @@ class ChairmanComponet extends Component
 {
     public function render()
     {
-        $chairmans = Chairman::where('hidden', false)->orderBy('updated_at', 'DESC')->get();
+        $chairmans = Chairman::where('hidden', false)->orderBy('created_at', 'DESC')->get();
         return view('livewire.chairman-componet', ['chairmans'=>$chairmans]);
     }
 }
