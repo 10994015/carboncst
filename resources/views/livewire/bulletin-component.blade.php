76cf189f43de6328ea546378d2a2db6270e7ce0b
10994015 <a0938599191@gmail.com>
2024-12-29 14:36:07 +0800

    回復為object

--- resources/views/livewire/bulletin-component.blade.php
+++ resources/views/livewire/bulletin-component.blade.php
@@ -8,13 +8,24 @@
         </div>
 
         <div class="pdf-container">
-            <iframe
-                src="/pdf/20241227.pdf"
+            <object
                 class="pdf-viewer"
-                style="width: 100%; height: 100%;"
-                scrolling="auto"
-                frameborder="0">
-            </iframe>
+                data="/pdf/20241227.pdf"
+                type="application/pdf">
+                <div class="fallback">
+                    <div class="fallback-content">
+                        <svg class="icon" viewBox="0 0 24 24" width="48" height="48">
+                            <path fill="currentColor" d="M12 2L2 12h3v8h14v-8h3L12 2zM12 16l-4-4h8l-4 4z"/>
+                        </svg>
+                        <p>無法直接顯示 PDF 文件</p>
+                        <a href="/pdf/20241227.pdf"
+                           class="btn btn-download"
+                           download>
+                            下載文件
+                        </a>
+                    </div>
+                </div>
+            </object>
         </div>
     </div>
 </div>
